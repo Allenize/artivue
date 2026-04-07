998adfb1cea3c3cf8684900993f36e7ae9961dde
Allenize <johnallenguerra@gmail.com>
2026-04-07 10:19:45 +0800

    logo change

--- src/components/ArtivueLogo.jsx
+++ src/components/ArtivueLogo.jsx
@@ -1,7 +1,7 @@
 export default function ArtivueLogo({ size = 80 }) {
   return (
     <img
-      src="/artivue-logo.svg"
+      src="/artivue-logo.png"
       alt="Artivue"
       width={size}
       height={size}
